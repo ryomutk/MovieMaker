3233110e87b13d30f4b954fe9893266fbf79eb68
ryomutk <72121144+ryomutk@users.noreply.github.com>
2023-09-08 14:01:53 +0900

    feat : add image samples

--- VIdeoMaker_master/VIdeoMaker/main.cpp
+++ VIdeoMaker_master/VIdeoMaker/main.cpp
@@ -4,7 +4,8 @@
 
 int main()
 {
-	MovieMaker::MakeVideo(".\\Mov\pool", ".\\Out\\pool.mp4");
+	MovieMaker::MakeVideo(".\\Sequences\\pool", ".\\Out\\pool.mp4");
+	MovieMaker::MakeVideo(".\\Sequences\\azuki", ".\\Out\\azuki.mp4", ".\\bg\\box.png");
 	//combine_aac_audio(".\\Out\\3_2.mp4", ".\\Sound\\Shiu_3_voice_5.wav", ".\\Out\\3_2_sound.mp4");
 
 	return 0;
